6a688b157d9dce0394cdc0aaca176d0ab8d7f06e
GrigoriyKruchinin <GrigoriyKruchinin>
2024-01-10 18:03:38 +0100

    update bot start and help commands

---
+++ bot/helpers/stickers_id.py
@@ -0,0 +1,5 @@
+#  получить ID стикера можно в тг боте @idstickerbot
+
+STICKER_DOG = (
+    "CAACAgIAAxkBAAELIitlnrqebILw4fRZ1TxmDvhm6SFo6AACfwEAAj0N6AS98XKpqDIlKDQE"
+)
--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,13 @@
 import os
 import asyncio
+import logging
 
 from dotenv import load_dotenv
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart, Command
 
-
+from bot.helpers.stickers_id import STICKER_DOG
 
 
 load_dotenv()
@@ -16,9 +17,47 @@ TOKEN = os.getenv('TOKEN')
 bot = Bot(TOKEN)
 dp = Dispatcher()
 
+
+@dp.message(CommandStart())
+async def start(message):
+    username = message.from_user.full_name
+    await message.answer(text=f'Привет, {username}!')
+    await message.answer(
+        text='Это тг бот для получения информации о свадьбе Гриши и Лены!'
+    )
+    await message.answer(
+        text=(
+            'Для получение общей информации воспользуйтесь командой /help'
+        )
+    )
+    await message.answer(
+        text=(
+            "Но я умею не только это 🫣 "
+            "Попробуйте все команды, не стесняйтесь!" 
+        )
+    )
+    await bot.send_sticker(chat_id=message.chat.id, sticker=STICKER_DOG)
+
+
+@dp.message(Command('help'))
+async def help(message):
+    await message.answer("Тут будет выводиться инфо о свадьбе")
+
+
 @dp.message()
+async def answer(message):
+    await message.reply(
+        text=(
+            'Для получения информации о свадьбе Гриши и Лены '
+            'воспользуйтесь встроенными командами!'
+        )
+    )
+    await bot.send_sticker(chat_id=message.chat.id, sticker=STICKER_DOG)
+
+
 async def main():
-    pass
+    logging.basicConfig(level=logging.INFO)
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
